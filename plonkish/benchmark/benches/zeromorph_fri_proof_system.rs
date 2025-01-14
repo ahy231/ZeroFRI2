3605b2e144c69ffd5a6308156a605b4edb0f6db6
Alok Kumar <rahul05alok@gmail.com>
2025-01-14 18:09:37 +0530

    add: bench data comparison across different mle-pcs

--- plonkish/benchmark/benches/zeromorph_fri_proof_system.rs
+++ plonkish/benchmark/benches/zeromorph_fri_proof_system.rs
@@ -298,7 +298,7 @@ impl Display for Circuit {
 /// Returns a tuple of (Vec<System>, Circuit, Range<usize>).
 fn parse_args() -> (Vec<System>, Circuit, Range<usize>) {
     let (systems, circuit, k_range) = args().chain(Some("".to_string())).tuple_windows().fold(
-        (Vec::new(), Circuit::VanillaPlonk, 10..19),
+        (Vec::new(), Circuit::VanillaPlonk, 10..24),
         |(mut systems, mut circuit, mut k_range), (key, value)| {
             match key.as_str() {
                 "--system" => match value.as_str() {
